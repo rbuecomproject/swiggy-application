5287cc1be4fac456fb7a10541a5fe9b777828dda
unknown <rbuecomproject@gmail.com>
2025-06-30 22:04:32 +0530

    basic skelton created

---
+++ src/main/java/com/rbu/swiggy/SwiggyApplictionApplication.java
@@ -0,0 +1,13 @@
+package com.rbu.swiggy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiggyApplictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiggyApplictionApplication.class, args);
+	}
+
+}
